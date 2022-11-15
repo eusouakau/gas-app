b6b5c89f7246d5299492675a73cf7ffc79cdb30b
Kauana Henriques <karoltvbl@gmail.com>
2022-11-15 17:06:33 -0300

    remove codigo inutil

--- app/src/main/java/com/example/gas_app/dao/AppDatabase.java
+++ app/src/main/java/com/example/gas_app/dao/AppDatabase.java
@@ -6,19 +6,15 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.gas_app.model.Gas;
 import com.example.gas_app.model.User;
 
 
-@Database(entities = { User.class, Gas.class }, version = 1)
+@Database(entities = { User.class }, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase appDatabase;
 
     public abstract UserDAO createUserDAO();
-
-    public abstract GasDAO createGasDAO();
-
 
     public static AppDatabase getInstance(Context context) {
         if(appDatabase == null) {
