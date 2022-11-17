22ec5fbd0c4ce1168a3369a971c4b34756228e49
Kauana Henriques <karoltvbl@gmail.com>
2022-11-17 11:39:27 -0300

    modifica estratégia

--- app/src/main/java/com/example/gas_app/adapter/ProdutosAdapter.java
+++ app/src/main/java/com/example/gas_app/adapter/ProdutosAdapter.java
@@ -1,52 +1,80 @@
 package com.example.gas_app.adapter;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gas_app.R;
+import com.example.gas_app.dao.AppDatabase;
+import com.example.gas_app.dao.PedidoDAO;
 import com.example.gas_app.mock.Produtos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class ProdutosAdapter extends RecyclerView.Adapter<LineHolder> {
+public class ProdutosAdapter extends RecyclerView.Adapter<ProdutosAdapter.MyViewHolder> {
 
-    private final List<Produtos> mProdutos;
-
-    public ProdutosAdapter(ArrayList produtos) {
-        mProdutos = produtos;
-    }
-
-    @Override
-    public LineHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new LineHolder(LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.gas_card, parent, false));
+    List<Produtos> listaProdutos = new ArrayList<>();
+    Context context;
+    public ProdutosAdapter(List<Produtos> produtos) {
+        this.listaProdutos = produtos;
     }
 
     @NonNull
     @Override
-    public LineHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ProdutosAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View itemList = LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.adapter_card_icones, viewGroup, false);
+
+        this.context = viewGroup.getContext();
+        return new ProdutosAdapter.MyViewHolder(itemList);
     }
 
     @Override
-    public void onBindViewHolder(LineHolder holder, int position) {
-        holder.title.setText(String.format(Locale.getDefault(), "%s, %d - %s",
-                mProdutos.get(position).getName(),
-                mProdutos.get(position).getAge(),
-                mProdutos.get(position).getCity()
-        ));
+    public void onBindViewHolder(@NonNull PedidoAdapter.MyViewHolder myViewHolder, @SuppressLint("RecyclerView") int position) {
+        Produtos p = listaProdutos.get(position);
+        myViewHolder.nome.setText(p.nome);
+        myViewHolder.preco.setText(p.preco);
+        myViewHolder.img.setText(p.img);
 
-        holder.moreButton.setOnClickListener(view -> updateItem(position));
-        holder.deleteButton.setOnClickListener(view -> removerItem(position));
+        Bundle bundle = new Bundle();
+        bundle.putInt("IMG", listaProdutos.get(position).img);
+        bundle.putString("NOME", listaProdutos.get(position).nome);
+        bundle.putString("PRECO", listaProdutos.get(position).preco);
+        myViewHolder.btnEdit.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_homeFragment, bundle));
     }
 
     @Override
     public int getItemCount() {
-        return mProdutos != null ? mProdutos.size() : 0;
+        return listaProdutos.size();
     }
 
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView nome;
+        TextView endereco;
+        ImageButton btnDelete;
+        ImageButton btnEdit;
+        public MyViewHolder(View itemView){
+            super(itemView);
+            nome = itemView.findViewById(R.id.textViewNome);
+            endereco = itemView.findViewById(R.id.textViewEndereco);
+            btnDelete = itemView.findViewById(R.id.btnExcluir);
+            btnEdit= itemView.findViewById(R.id.btnEditar);
+        }
+    }
 }
+
