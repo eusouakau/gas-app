8249d1d4d19e5448aa7c29313d6d76728945bb6d
Kauana Henriques <karoltvbl@gmail.com>
2022-11-17 00:34:40 -0300

    corrige navegação

--- app/src/main/java/com/example/gas_app/activity/MainActivity.java
+++ app/src/main/java/com/example/gas_app/activity/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_cadastrarFragment, R.id.nav_listarFragment)
+                R.id.nav_home, R.id.nav_cadastrarFragment, R.id.nav_historicoFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -55,15 +55,17 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
                 // User chose the "Settings" item, show the app settings UI...
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Titulo");
+                builder.setTitle("Segurança");
                 //define a mensagem
-                builder.setMessage("abrindo configurações")
+                builder.setMessage("1. Manusear botijões de gás com cuidado, evitando que caiam ou sofram pancadas;\n" +
+                                "2. Os botijões devem ser guardados em locais limpos, ventilados, livres de óleo e graxa, protegidos contra chuva, sol, e outras fontes de calor;\n" +
+                                "3. Botijões de gás domésticos não devem ficar juntos do fogão. Precisam estar fora da casa e conectados com tubulações metálicas;\n" +
+                                "4. Caso o gás esteja instalado dentro de casa e ele vier a vazar, não risque fósforo e não acenda ou apague luzes. Chame os bombeiros (193) e se possível retire o botijão da sua casa. Abra as portas e janelas, corte a energia no relógio e fique longe do local onde o gás está vazando;\n" +
+                                "5. Ao instalar um novo botijão use espuma de sabão para testar se há vazamentos;\n" +
+                                "6. Jamais use fogo para tal propósito, mas lembre-se: o sabão não deve ser usado para vedar vazamentos;\n" +
+                                "7. Ao acender um forno de fogão, riscar primeiro o fósforo e abrir o gás depois;\n" +
+                                "Se a casa ficar desocupada por um período prolongado, feche o registro de gás.")
                         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                //colocar a ação aqui
-                            }
-                        })
-                        .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 //colocar a ação aqui
                             }
