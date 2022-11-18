5358045214a519e4ecd0bd0fec0a569f835d5dc3
Kauana Henriques <karoltvbl@gmail.com>
2022-11-18 02:29:32 -0300

    adiciona props que faltavam

--- app/src/main/java/com/example/gas_app/model/Pedido.java
+++ app/src/main/java/com/example/gas_app/model/Pedido.java
@@ -1,6 +1,7 @@
 package com.example.gas_app.model;
 
 import java.io.Serializable;
+
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -10,6 +11,11 @@ public class Pedido implements Serializable {
     private int id;
     private String nome;
     private String endereco;
+    private String formaPag;
+    private String nomeProd;
+    private String total;
+    private String qtd;
+    private String preco;
 
     public Pedido() {
     }
@@ -26,6 +32,10 @@ public class Pedido implements Serializable {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getEndereco() {
         return endereco;
     }
@@ -34,16 +44,43 @@ public class Pedido implements Serializable {
         this.endereco = endereco;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getFormaPag() {
+        return formaPag;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", endereco='" + endereco + '\'' +
-                '}';
+    public void setFormaPag(String formaPag) {
+        this.formaPag = formaPag;
+    }
+
+    public String getNomeProd() {
+        return nomeProd;
+    }
+
+    public void setNomeProd(String nomeProd) {
+        this.nomeProd = nomeProd;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(String qtd) {
+        this.qtd = qtd;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
     }
 }
