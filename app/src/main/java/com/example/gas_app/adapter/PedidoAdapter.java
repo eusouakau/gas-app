8d1ff407323e031535de323360e00fa6ac1d6ed0
Kauana Henriques <karoltvbl@gmail.com>
2022-11-18 02:29:56 -0300

    ajusta adapter

--- app/src/main/java/com/example/gas_app/adapter/PedidoAdapter.java
+++ app/src/main/java/com/example/gas_app/adapter/PedidoAdapter.java
@@ -4,18 +4,15 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gas_app.R;
@@ -37,7 +34,7 @@ public class PedidoAdapter extends RecyclerView.Adapter<PedidoAdapter.MyViewHold
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
         View itemList = LayoutInflater.from(viewGroup.getContext()).
-                inflate(R.layout.adapter_card_icones, viewGroup, false);
+                inflate(R.layout.adapter_card, viewGroup, false);
 
         this.context = viewGroup.getContext();
         return new MyViewHolder(itemList);
@@ -46,19 +43,16 @@ public class PedidoAdapter extends RecyclerView.Adapter<PedidoAdapter.MyViewHold
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, @SuppressLint("RecyclerView") int position) {
         Pedido p = listaPedidos.get(position);
-        myViewHolder.nome.setText(p.getNome());
-        myViewHolder.endereco.setText(p.getEndereco());
-        myViewHolder.btnDelete.setOnClickListener(new View.OnClickListener() {
+        myViewHolder.nomeProd.setText(p.getNomeProd());
+        myViewHolder.total.setText(p.getTotal());
+        myViewHolder.qtd.setText(p.getQtd());
+        myViewHolder.btnExcluir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 removerItem(position);
             }
         });
-        Bundle bundle = new Bundle();
-        bundle.putInt("ID", listaPedidos.get(position).getId());
-        bundle.putString("NOME", listaPedidos.get(position).getNome());
-        bundle.putString("END", listaPedidos.get(position).getEndereco());
-        myViewHolder.btnEdit.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_editarFragment, bundle));
+
     }
 
     @Override
@@ -68,8 +62,8 @@ public class PedidoAdapter extends RecyclerView.Adapter<PedidoAdapter.MyViewHold
 
     public void removerItem(final int position) {
         new AlertDialog.Builder(context)
-                .setTitle("Deletando usuário")
-                .setMessage("Tem certeza que deseja deletar este usuario?")
+                .setTitle("Exclusão de registro de pedido.")
+                .setMessage("Tem certeza que deseja deletar este registro?")
                 .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -92,16 +86,18 @@ public class PedidoAdapter extends RecyclerView.Adapter<PedidoAdapter.MyViewHold
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView nome;
-        TextView endereco;
-        ImageButton btnDelete;
-        ImageButton btnEdit;
+        TextView nomeProd;
+        TextView total;
+        TextView qtd;
+        ImageButton btnExcluir;
+
         public MyViewHolder(View itemView){
             super(itemView);
-            nome = itemView.findViewById(R.id.textViewNome);
-            endereco = itemView.findViewById(R.id.textViewEndereco);
-            btnDelete = itemView.findViewById(R.id.btnExcluir);
-            btnEdit= itemView.findViewById(R.id.btnEditar);
+            nomeProd = itemView.findViewById(R.id.textViewNomeProd);
+            total = itemView.findViewById(R.id.textViewTotal);
+            qtd = itemView.findViewById(R.id.textViewQtd);
+            btnExcluir = itemView.findViewById(R.id.btnExcluir);
+            
         }
     }
 }
