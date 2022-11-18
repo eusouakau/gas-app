379db892960db66ee582fe93f4fd9ec597a0aa2d
Kauana Henriques <karoltvbl@gmail.com>
2022-11-18 02:28:47 -0300

    adiciona mock

--- app/src/main/java/com/example/gas_app/mock/MockProduto.java
+++ app/src/main/java/com/example/gas_app/mock/MockProduto.java
@@ -2,7 +2,7 @@ package com.example.gas_app.mock;
 
 import com.example.gas_app.R;
 
-public class Produtos {
+public class MockProduto {
     public static String[] nome = { "Botijão 8kg", "Botijão 30kg", "Botijão 45kg" };
     public static Double[] preco = { 70.00, 105.00, 300.00 };
     public static Integer[] img = { R.drawable.gas8, R.drawable.gas30, R.drawable.gas45 };
